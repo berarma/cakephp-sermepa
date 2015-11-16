cc6778c4977fddf7ec0e3a3e981937d1e1c01fed
jaykay-design <jay@jaykay-design.com>
2015-11-16 12:53:41 +0100

    using the RedsysAPI to create signatures and post data

--- Lib/Sermepa.php
+++ Lib/Sermepa.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *
  * CakePHP Library Object to interact with the Sermepa TPV service
@@ -22,70 +23,68 @@
  * @link http://bernatarlandis.com
  * @license http://www.gnu.org/licenses/gpl-2.0.html GPLv2
  */
+include 'apiRedsys.php';
 
 class Sermepa extends Object {
 
-	protected $_settings;
+    protected $_settings;
+    protected $_notification;
+    private $_hash_version = 'HMAC_SHA256_V1';
 
-	protected $_notification;
+    public function __construct($settings) {
+        $this->_settings = $settings;
+    }
 
-	public function __construct($settings) {
-		$this->_settings = $settings;
-	}
+    public function getPostUrl() {
+        return $this->_settings->serviceUrl;
+    }
 
-	public function getPostUrl() {
-		return $this->_settings->serviceUrl;
-	}
+    public function getPostData($order, $amount, $transactionType = 0) {
 
-	public function getPostData($order, $amount, $transactionType = 0) {
-		$order = str_pad($order, 12, '0', STR_PAD_LEFT);
-		if ($this->_settings->extendedSignature) {
-			$signature = hash("sha256", $amount . $order . $this->_settings->merchantCode . $this->_settings->currency . $transactionType . $this->_settings->merchantUrl . $this->_settings->secretKey);
-		} else {
-			$signature = hash("sha256", $amount . $order . $this->_settings->merchantCode . $this->_settings->currency . $this->_settings->secretKey);
-		}
-		return array(
-			'Ds_Merchant_Amount' => $amount,
-			'Ds_Merchant_Currency' => $this->_settings->currency,
-			'Ds_Merchant_Order' => $order,
-			'Ds_Merchant_MerchantCode' => $this->_settings->merchantCode,
-			'Ds_Merchant_MerchantURL' => $this->_settings->merchantUrl,
-			'Ds_Merchant_UrlOK' => $this->_settings->urlOk,
-			'Ds_Merchant_UrlKO' => $this->_settings->urlKo,
-			'Ds_Merchant_MerchantName' => $this->_settings->merchantName,
-			'Ds_Merchant_ConsumerLanguage' => $this->_settings->consumerLanguage,
-			'Ds_Merchant_MerchantSignature' => $signature,
-			'Ds_Merchant_Terminal' => $this->_settings->terminal,
-			'Ds_Merchant_TransactionType' => $transactionType,
-		);
-	}
+        $api = new RedsysAPI;
 
-/**
- * @throws CakeException
- */
-	public function getNotificationData($data) {
-		$this->_notification = new stdClass();
-		$this->_notification->date = $data['Ds_Date'];
-		$this->_notification->hour = $data['Ds_Hour'];
-		$this->_notification->amount = $data['Ds_Amount'];
-		$this->_notification->currency = $data['Ds_Currency'];
-		$this->_notification->order = $data['Ds_Order'];
-		$this->_notification->merchantCode = $data['Ds_MerchantCode'];
-		$this->_notification->terminal = $data['Ds_Terminal'];
-		$this->_notification->signature = $data['Ds_Signature'];
-		$this->_notification->response = $data['Ds_Response'];
-		$this->_notification->merchantData = $data['Ds_MerchantData'];
-		$this->_notification->securePayment = $data['Ds_SecurePayment'];
-		$this->_notification->transactionType = $data['Ds_TransactionType'];
-		$this->_notification->cardCountry = $data['Ds_Card_Country'];
-		$this->_notification->authorisationCode = $data['Ds_AuthorisationCode'];
-		$this->_notification->consumerLanguage = $data['Ds_ConsumerLanguage'];
-		$this->_notification->cardType = $data['Ds_Card_Type'];
-		$signature = hash("sha256", $this->_notification->amount . $this->_notification->order . $this->_notification->merchantCode . $this->_notification->currency . $this->_notification->response . $this->_settings->secretKey);
-		if (strtolower($this->_notification->signature) !== $signature) {
-			throw new CakeException("Invalid signature in Sermepa notification.");
-		}
-		return $this->_notification;
-	}
+        $api->setParameter('Ds_Merchant_Amount', $amount);
+        $api->setParameter('Ds_Merchant_Currency', $this->_settings->currency);
+        $api->setParameter('Ds_Merchant_Order', $order);
+        $api->setParameter('Ds_Merchant_MerchantCode', $this->_settings->merchantCode);
+        $api->setParameter('Ds_Merchant_MerchantURL', $this->_settings->merchantUrl);
+        $api->setParameter('Ds_Merchant_UrlOK', $this->_settings->urlOk);
+        $api->setParameter('Ds_Merchant_UrlKO', $this->_settings->urlKo);
+        $api->setParameter('Ds_Merchant_MerchantName', $this->_settings->merchantName);
+        $api->setParameter('Ds_Merchant_ConsumerLanguage', $this->_settings->consumerLanguage);
+        $api->setParameter('Ds_Merchant_Terminal', $this->_settings->terminal);
+        $api->setParameter('Ds_Merchant_TransactionType', $transactionType);
+
+        $parameters = $api->createMerchantParameters();
+        $signature = $api->createMerchantSignature($this->_settings->secretKey);
+
+        return array(
+            'Ds_SignatureVersion' => $this->_hash_version,
+            'Ds_MerchantParameters' => $parameters,
+            'Ds_Signature' => $signature
+        );
+    }
+
+    /**
+     * @throws CakeException
+     */
+    public function getNotificationData($data) {
+        $version = $data["Ds_SignatureVersion"];
+        if ($version !== $this->_hash_version) {
+            throw new CakeException("Invalid signature version ($version) received from Sermepa.");
+        }
+
+        $api = new RedsysAPI;
+
+        $paramters = $api->decodeMerchantParameters($data);
+        $signature = $miObj->createMerchantSignatureNotif($this->_settings->secretKey, $data);
+
+        if ($data["Ds_Signature"] !== $signature) {
+            throw new CakeException("Invalid signature in Sermepa notification.");
+        }
+
+        $this->_notification = json_decode($paramters);
+        return $this->_notification;
+    }
 
 }
